04b76b455975adeb5562bce818e039fff1095093
Billie <billiemj@gmail.com>
2020-07-23 20:03:25 -0500

    add

--- midstates/src/app/app.module.ts
+++ midstates/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
+import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +10,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    MDBBootstrapModulesPro.forRoot(),
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    MDBSpinningPreloader
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
