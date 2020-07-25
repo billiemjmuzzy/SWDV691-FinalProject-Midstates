d84c7db2135ca2b15142d226e586b9011e6e34fb
Billie <billiemj@gmail.com>
2020-07-24 20:08:29 -0500

    form
    created form

--- midstates/src/app/products/inventory-create/inventory-create.component.ts
+++ midstates/src/app/products/inventory-create/inventory-create.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { UploadFile, UploadInput, UploadOutput } from 'ng-uikit-pro-standard';
+import { humanizeBytes } from 'ng-uikit-pro-standard';
 
 @Component({
   selector: 'app-inventory-create',
@@ -6,14 +8,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inventory-create.component.css']
 })
 export class InventoryCreateComponent implements OnInit {
+  formData: FormData;
+  files: UploadFile[];
+  uploadInput: EventEmitter<UploadInput>;
+  humanizeBytes: Function;
+  dragOver: boolean;
 
-  optionsSelect: Array<any>;
+  constructor() {
+    this.files = [];
+    this.uploadInput = new EventEmitter<UploadInput>();
+    this.humanizeBytes = humanizeBytes;
+  }
+
+  showFiles() {
+    let files = '';
+    for (let i = 0; i < this.files.length; i++) {
+      files += this.files[i].name;
+      if (!(this.files.length - 1 === i)) {
+        files += ',';
+      }
+    }
+    return files;
+  }
+
+  startUpload(): void {
+    const event: UploadInput = {
+      type: 'uploadAll',
+      url: 'your-path-to-backend-endpoint',
+      method: 'POST',
+      data: { foo: 'bar' },
+    };
+    this.files = [];
+    this.uploadInput.emit(event);
+  }
+
+  cancelUpload(id: string): void {
+    this.uploadInput.emit({ type: 'cancel', id: id });
+  }
+
+  onUploadOutput(output: UploadOutput | any): void {
+
+    if (output.type === 'allAddedToQueue') {
+    } else if (output.type === 'addedToQueue') {
+      this.files.push(output.file); // add file to array when added
+    } else if (output.type === 'uploading') {
+      // update current data in files array for uploading file
+      const index = this.files.findIndex(file => file.id === output.file.id);
+      this.files[index] = output.file;
+    } else if (output.type === 'removed') {
+      // remove file from array when removed
+      this.files = this.files.filter((file: UploadFile) => file !== output.file);
+    } else if (output.type === 'dragOver') {
+      this.dragOver = true;
+    } else if (output.type === 'dragOut') {
+    } else if (output.type === 'drop') {
+      this.dragOver = false;
+    }
+    this.showFiles();
+  }
+
   ngOnInit() {
-    this.optionsSelect = [
-      { value: 'Feedback', label: 'Feedback' },
-      { value: 'Report a bug', label: 'Report a bug' },
-      { value: 'Feature request', label: 'Feature request' },
-      { value: 'Other stuff', label: 'Other stuff' },
-    ];
+
   }
 }
