3c91b6c71ae07544703eb3aabac9df1cc332d601
Billie <billiemj@gmail.com>
2020-07-27 16:05:26 -0500

    add angular

--- midstates/backend/app.js
+++ midstates/backend/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/inventories", (req, res, next) => {
-  const post = req.body;
+  const inventory = req.body;
   console.log(inventory);
   //everything is Ok, a new resource was created.
   res.status(201).json({
@@ -32,7 +32,7 @@ app.post("/api/inventories", (req, res, next) => {
   });
 });
 
-app.use("/api/inventories", (req, res, next) => {
+app.get("/api/inventories", (req, res, next) => {
   const inventories = [
     {
       id: 1,
--- midstates/src/app/products/inventories.service.ts
+++ midstates/src/app/products/inventories.service.ts
@@ -14,8 +14,12 @@ export class InventoriesService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * Get Inventories:
+   * Gets all Inventory items.
+   */
   getInventories() {
-    return this.http.get<{message: string, inventories: Inventory[] }>('http://localhost:3000/api/inventories')
+    return this.http.get<{ message: string, inventories: Inventory[] }>('http://localhost:3000/api/inventories')
       .subscribe((inventoryData) => {
         this.inventories = inventoryData.inventories;
         this.inventoriesUpdated.next([...this.inventories]);
@@ -27,11 +31,36 @@ export class InventoriesService {
     return this.inventoriesUpdated.asObservable();
   }
 
+  /**
+   * Add Inventory:
+   * Posts all inventory items
+   * @param image string
+   * @param brand string
+   * @param year number
+   * @param hours number
+   * @param condition string
+   * @param serial string
+   * @param price number
+   * @param description string
+   */
   addInventory(image: string, brand: string, year: number, hours: number, condition: string, serial: string, price: number, description: string) {
-    const inventory: Inventory = {id: null, image: image, brand: brand, year: year, hours: hours, condition: condition, serial: serial, price: price, description: description }
-    this.inventories.push(inventory);
-    this.inventoriesUpdated.next([...this.inventories]);
+    const inventory: Inventory = {
+      id: null,
+      image: image,
+      brand: brand,
+      year: year,
+      hours: hours,
+      condition: condition,
+      serial: serial,
+      price: price,
+      description: description
+    };
+    this.http.post<{ message: string }>('http://localhost:3000/api/inventories', inventory)
+      .subscribe((responseData) => {
+        console.log(responseData.message);
+        this.inventories.push(inventory);
+        this.inventoriesUpdated.next([...this.inventories]);
+
+      });
   }
-
-
 }
