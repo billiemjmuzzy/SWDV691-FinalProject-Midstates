0be14efefd5c278795d4a089c0ee612a4bbebc6b
Billie <billiemj@gmail.com>
2020-08-13 11:33:19 -0500

    jwt
    implementing SPA authentication

--- midstates/backend/routes/user.js
+++ midstates/backend/routes/user.js
@@ -1,32 +1,67 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = requite("jsonwebtoken");
+
 
 const User = require("../models/user");
 
 const router = express.Router();
 
-router.post("/new-user", (req, res, next)=> {
-  bcrypt.hash(req.body.password, 10)
-  .then(hash =>{
+router.post("/new-user", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
     const user = new User({
       email: req.body.email,
-      password: hash
+      password: hash,
     });
-    user.save()
+    user
+      .save()
+      .then((result) => {
+        res.status(201).json({
+          message: "User created",
+          result: result,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: err,
+        });
+      });
+  });
+});
+
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password);
+    })
     .then(result => {
-      res.status(201).json({
-        message: 'User created',
-        result: result
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      const token = jwt.sign(
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        // TODO update secret so it meets standards
+        "secret_this_should_be_longer",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({
+        token: token
       });
     })
     .catch(err => {
-      res.status(500).json({
-        error: err
+      return res.status(401).json({
+        message: "Auth failed"
       });
-
     });
-
-  });
 });
 
 module.exports = router;
