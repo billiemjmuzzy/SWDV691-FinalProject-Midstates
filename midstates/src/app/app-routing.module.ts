89e9f5f6476031a42a057520b6c25b6bd3eecf2f
Billie <billiemj@gmail.com>
2020-08-13 19:24:11 -0500

    auth guard

--- midstates/src/app/app-routing.module.ts
+++ midstates/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './auth/auth.guard';
 import { NewUserComponent } from './auth/new-user/new-user.component';
 import { LoginComponent } from './auth/login/login.component';
 import { InventoryCreateComponent } from './products/inventory-create/inventory-create.component';
@@ -7,16 +8,17 @@ import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   { path: '', component: InventoryViewComponent },
-  { path: 'create', component: InventoryCreateComponent },
-  { path: 'edit/:inventoryId', component: InventoryCreateComponent },
+  { path: 'create', component: InventoryCreateComponent, canActivate: [AuthGuard] },
+  { path: 'edit/:inventoryId', component: InventoryCreateComponent, canActivate: [AuthGuard]},
   { path: 'login', component: LoginComponent },
-  { path: 'new-user', component: NewUserComponent },
+  { path: 'new-user', component: NewUserComponent, canActivate: [AuthGuard] },
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
+
 export class AppRoutingModule { }
